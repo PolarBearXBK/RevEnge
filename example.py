d7fc66f8d28f0c6ab3fe3e09c223adf0bc4e34f3
PolarBearXBK <bxiong@opploans.com>
2020-08-07 14:47:59 -0400

    0807 example update

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ from contributions import get_contribution
 # The function is capable of fitting
 test1 = get_contribution(
     model=M,  # Xgboost model
-    df=params,  # Only the features
+    df=params,  # Dataframe with only the features
     calculate=True,  # Tells function to predict its own labels
     percentiles=10  # Tells function how many sampling points it wants
 )
@@ -12,8 +12,8 @@ test1 = get_contribution(
 # If the
 test2 = get_contribution(
     model=M,  # Xgboost model
-    df=params,  # Only the features
-    target_df=target,  # Only the labels
+    df=params,  # Dataframe with only the features
+    target_df=target,  # Dataframe with only the labels
     calculate=False,  # Tells function not to predict its own labels
     percentiles=10  # Tells function how many sampling points it wants
 )
@@ -21,7 +21,7 @@ test2 = get_contribution(
 # If the labels are already inside the feature dataframe
 test3 = get_contribution(
     model=M,  # Xgboost model
-    df=dataframe,  # Features and labels
+    df=dataframe,  # Dataframe with features and labels
     target='TargetColumnName',  # The column where the labels are stored
     calculate=False,  # Tells function not to predict its own labels
     percentiles=10  # Tells function how many sampling points it wants
